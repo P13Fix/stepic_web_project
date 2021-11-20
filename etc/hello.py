1b5a1798630db18d755e55c69a6bc8b9f848587a
P13Fix <74654466+P13Fix@users.noreply.github.com>
2021-11-20 23:15:00 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,9 +1,5 @@
 def app(environ, start_response):
-    data = b'Hello, World!\n'
     status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    return iter([data])
+    start_response(status, [('Content-type', 'text/plain; charset=utf-8')])
+    resp = "\n".join(environ.get('QUERY_STRING').split("&"))
+    return resp
